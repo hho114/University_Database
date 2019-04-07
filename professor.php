3d20e5a96b859f6546819690c34bfd1fd990f6f3
Alex <huyho114@gmail.com>
2019-04-06 17:47:05 -0700

    update index.html, professor.php; create course.php, count_student.php

--- professor.php
+++ professor.php
@@ -2,11 +2,15 @@
 <body>
 
 <?php
+$servername = "ecsmysql";
+$username = "cs322t15";
+$password = "huythanh";
+$dbname = "myDB";
 // username and password need to be replaced by your username and password
 // $link = mysql_connect('ecsmysql', 'username', 'password');
-
-$link = mysql_connect('ecsmysql', 'cs322t15', 'huythanh');
-
+$professor_name = "";
+$ssn;
+$link = mysql_connect($servername, $username, $password);
 if (!$link)
 {
   die('Could not connect: ' . mysql_error());
@@ -14,10 +18,12 @@ if (!$link)
 }
  echo 'Connected successfully<p>';
 
+ mysql_select_db($username,$link);
 
- mysql_select_db("username",$link);
+// Validation the inputs
 
- $result = mysql_query("SELECT * FROM STUDENT",$link);
+
+ $result = mysql_query("SELECT  FROM STUDENT",$link);
 
  // for($i=0; $i<mysql_numrows($result); $i++)
  // {
