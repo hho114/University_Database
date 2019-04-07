46f0f823b8b2850348377f199adb7e2df10762ad
Alex <huyho114@gmail.com>
2019-04-06 23:10:33 -0700

    update Table.sql

--- Table.sql
+++ Table.sql
@@ -19,8 +19,9 @@ Create Table Professor
 -- Create Table Degree
 Create Table Degree
 (
-  degree_prof_id numeric(9) not null foreign key references Professor(prof_id),
-  degree_name varchar (20) not null
+  degree_prof_id numeric(9) not null,
+  degree_name varchar (20) not null,
+  foreign key(degree_prof_id) references Professor(prof_id)
 );
 
 -- Create table student
@@ -31,7 +32,8 @@ Create Table Student
   stu_last_name varchar(20) not null,
   stu_address varchar(60) not null,
   stu_phone_number numeric(10) not null,
-  stu_major_dep_id numeric(5) not null foreign key references Department(dep_id),
+  stu_major_dep_id numeric(5) not null,
+  foreign key (stu_major_dep_id) references Department(dep_id),
 );
 
 -- Create Minor Major Table
@@ -39,9 +41,10 @@ Create Table Student
 Create Table Minor
 (
 
-  minor_stu_cwid numeric(10) not null foreign key references Student(stu_cwid),
-  minor_dep_id numeric(5) not null foreign key references Department(dep_id)
-
+  minor_stu_cwid numeric(10) not null ,
+  minor_dep_id numeric(5) not null,
+foreign key(minor_stu_cwid) references Student(stu_cwid),
+foreign key (minor_dep_id) references Department(dep_id)
 );
 
 -- Create table Department
@@ -51,7 +54,8 @@ Create Table Department
   dep_name varchar(20) not null,
   dep_phone_number numeric(10) not null,
   dep_location varchar(60) not null,
-  dep_chair_prof_id numeric(9) not null foreign key references Professor(prof_id)
+  dep_chair_prof_id numeric(9) not null,
+  foreign key (dep_chair_prof_id)references Professor(prof_id)
 
 );
 
@@ -62,15 +66,19 @@ Create Table Course
   course_title varchar(50) not null,
   course_textbook varchar(60) not null,
   course_units int not null,
-  course_dep_id numeric(5) not null foreign key references Department(dep_id)
+  course_dep_id numeric(5) not null,
+  foreign key (course_dep_id)references Department(dep_id)
 
 );
 
 -- create table Prerequisite
 Create Table Prerequisite
 (
-  upper_course_id varchar(10) not null foreign key references Course(course_id),
-  lower_course_id varchar(10) foreign key references Course(course_id)
+  upper_course_id varchar(10) not null,
+  lower_course_id varchar(10) ,
+  foreign key(upper_course_id) references Course(course_id),
+  foreign key(lower_course_id) references Course(course_id)
+
 );
 
 -- create table Section
@@ -82,14 +90,18 @@ Create Table Section
   section_meeting_day varchar(20) not null,
   section_beginning_day varchar(20) not null,
   section_ending_day varchar(20) not null,
-  section_prof_id numeric(9) not null foreign key references Professor(prof_id),
-  section_course_id varchar(10) not null foreign key references Course(course_id)
+  section_prof_id numeric(9) not null ,
+  section_course_id varchar(10) not null ,
+  foreign key(section_prof_id) references Professor(prof_id),
+  foreign key(section_course_id) references Course(course_id)
 );
 
 -- create table Enrollment
 Create Table Enrollment
 (
   enroll_grade enum('A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F') not null,
-  enroll_section_id not null foreign key references Section(section_id),
-  enroll_stu_cwid not null foreign key references Student(stu_cwid)
+  enroll_section_id not null ,
+  enroll_stu_cwid not null ,
+  foreign key(enroll_section_id) references Section(section_id),
+  foreign key (enroll_stu_cwid)references Student(stu_cwid)
 );
