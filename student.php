af40d03855cbdffb587777a720eec59a29e61a32
hho114 <hho114@csu.fulleton.edu>
2019-04-16 22:47:19 -0700

    update student php

--- student.php
+++ student.php
@@ -21,37 +21,53 @@ if (!$link)
 
 $stu_id = $_POST["student_cwid"];
 //use SQL SELECT to query data
-$select = "SELECT course_title, enroll_grade
+$sql = "SELECT course_title, enroll_grade
 	FROM Enrollment, Course, Section, Student
 	WHERE  stu_cwid = enroll_stu_cwid AND enroll_section_id = section_id
   AND section_course_id = course_id AND enroll_stu_cwid =  '$stu_id'";
 
 
 
- $result = mysql_query($select,$link);
-  if(!$result)//check if find data correct
-  {
-    die('Could not get data: '. mysql_error());
-  }
+ $result = mysql_query($sql,$link);
 
-  for($i=0; $i<mysql_numrows($result); $i++)
+ if (!$result) {
+     echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+     exit;
+ }
+
+ if (mysql_num_rows($result) == 0) {
+     echo "No rows found, nothing to print so am exiting";
+     exit;
+ }
+ $border_data = "<td style='width:150px;border:1px solid black;'>";
+ $border_header = "<th style='width:150px;border:1px solid black;'>";
+
+ echo "<table style='border: solid 1px black;'>";
+ echo "
+ <tr>
+ " .$border_header. "Course Title</th>
+   " .$border_header. "Grade</th>
+
+ </tr>";
+  while($row = mysql_fetch_assoc($result))
   {
-   echo "Course Title: ", mysql_result($result,$i, course_title), "<br>";
-   echo "Grade: ", mysql_result($result,$i, enroll_grade), "<br>", "<br>";
+  echo "
+  <tr>
+  " .$border_data.$row["course_title"]."</td>
+  " .$border_data.$row["enroll_grade"]."</td>
+
+  </tr>";
   }
+  echo "</table>";
+
+
+
 
  echo "Fetched data successfully\n";
  mysql_close($link);
 
  ?>
 
- <button onclick="goBack()">Go Back</button>
-
- <script>
- function goBack() {
-     window.history.back();
- }
- </script>
 
  </body>
  </html>
