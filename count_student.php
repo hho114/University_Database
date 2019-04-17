ff75e06db0badcb21b84a03d0a59e69548f20575
hho114 <hho114@csu.fulleton.edu>
2019-04-16 22:58:11 -0700

    update count student

--- count_student.php
+++ count_student.php
@@ -23,13 +23,13 @@ $course_id = $_POST["course_num"];
 $section_id = $_POST["section_num"];
 // TO DO: fix the sql command to sastify section b for Professor,
 // check project assigment for more info.
-$select = "SELECT DISTINCT enroll_grade, course_title, Count(enroll_grade) as 'Count'
+$sql = "SELECT DISTINCT enroll_grade, course_title, Count(enroll_grade) as 'Count'
  FROM Course , Section , Enrollment
  WHERE course_id = section_course_id
  AND enroll_section_id = section_id
  AND course_id = '$course_id' AND section_id = '$section_id' GROUP BY enroll_grade;";
 
- $result = mysql_query($select,$link);
+ $result = mysql_query($sql,$link);
 
  if (!$result) {
      echo "Could not successfully run query ($sql) from DB: " . mysql_error();
@@ -41,7 +41,7 @@ $select = "SELECT DISTINCT enroll_grade, course_title, Count(enroll_grade) as 'C
      exit;
  }
 
- 
+
  $border_data = "<td style='width:150px;border:1px solid black;'>";
  $border_header = "<th style='width:150px;border:1px solid black;'>";
 
@@ -67,13 +67,7 @@ $select = "SELECT DISTINCT enroll_grade, course_title, Count(enroll_grade) as 'C
 
  ?>
 
- <button onclick="goBack()">Go Back</button>
 
- <script>
- function goBack() {
-     window.history.back();
- }
- </script>
 
  </body>
  </html>
