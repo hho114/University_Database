52f32380d478b8f7763b5d06335d4b39e088fa83
hho114 <hho114@csu.fulleton.edu>
2019-04-16 17:27:34 -0700

    update course php

--- course.php
+++ course.php
@@ -22,7 +22,7 @@ if (!$link)
 $course_id =$_POST["stu_course_num"];
  // TO DO: fix the sql command to sastify section a for Student,
  // check project assigment for more info.
-$select = "SELECT course_id, course_title, section_id, section_class_room, section_meeting_day,
+$sql = "SELECT course_id, course_title, section_id, section_class_room, section_meeting_day,
 		section_beginning_day, section_ending_day, (section_amount_of_seat-COUNT(enroll_stu_cwid)) as 'Seats'
 
 	FROM Course, Enrollment, Section
@@ -30,24 +30,50 @@ $select = "SELECT course_id, course_title, section_id, section_class_room, secti
 	AND course_id = section_course_id
 	AND course_id = '$course_id' GROUP BY section_id";
 
- $result = mysql_query($select,$link);
-
- if(!$result)//check if find data correct
- {
-   die('Could not get data: '. mysql_error());
- }
+ $result = mysql_query($sql,$link);
 
 
-   for($i=0; $i<mysql_numrows($result); $i++)
+  if (!$result) {
+      echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+      exit;
+  }
+
+  if (mysql_num_rows($result) == 0) {
+      echo "No rows found, nothing to print so am exiting";
+      exit;
+  }
+
+
+  $border_data = "<td style='width:150px;border:1px solid black;'>";
+  $border_header = "<th style='width:150px;border:1px solid black;'>";
+
+  echo "<table style='border: solid 1px black;'>";
+  echo "
+  <tr>
+  " .$border_header. "Course Title </th>
+    " .$border_header. "Section Number </th>
+    " .$border_header. "Classroom Location </th>
+    " .$border_header. "Meeting Days </th>
+    " .$border_header. "Start Date </th>
+    " .$border_header. "End Date </th>
+    " .$border_header. "Number of Seats </th>
+
+  </tr>";
+   while($row = mysql_fetch_assoc($result))
    {
-    echo "Course Title: ", mysql_result($result,$i,course_title), "<br>";
-    echo "Section Number: ", mysql_result($result,$i, section_id), "<br>";
-    echo "Classroom Location: ", mysql_result($result,$i, section_class_room), "<br>";
-    echo "Meeting Days: ", mysql_result($result,$i, section_meeting_day), "<br>";
-    echo "Start Time: ", mysql_result($result,$i, section_beginning_day), "<br>";
-    echo "End Time: ", mysql_result($result,$i, section_ending_day), "<br>";
-    echo "Number of Seats: ", mysql_result($result,$i, "Seats"), "<br>", "<br>";
-    }
+   echo "
+   <tr>
+   " .$border_data.$row["course_title"]."</td>
+   " .$border_data.$row["section_id"]."</td>
+   " .$border_data.$row["section_class_room"]."</td>
+   " .$border_data.$row["section_meeting_day"]."</td>
+   " .$border_data.$row["section_beginning_day"]."</td>
+   " .$border_data.$row["section_ending_day"]."</td>
+   " .$border_data.$row["Seats"]."</td>
+
+   </tr>";
+   }
+   echo "</table>";
 
  mysql_close($link);
 
