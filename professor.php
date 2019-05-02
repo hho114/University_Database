33138d2ed2bb37d83955c7e117cf4dd7b41acca8
hho114 <hho114@csu.fulleton.edu>
2019-05-02 04:10:05 -0700

    update index

--- professor.php
+++ professor.php
@@ -58,8 +58,10 @@ $sql = "SELECT course_title, section_class_room, section_meeting_day, section_be
   <div class="container-fluid bg-success text-center">
     <h3>Professor Assign Courses</h3>
   </div>
-  
+
 <div class="container-fluid text-center">
+
+
   <table class="table table-striped table-bordered">
      <thead>
          <tr class="info text-center">
