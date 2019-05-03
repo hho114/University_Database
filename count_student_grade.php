412612220c4044810a2d933b829dd956a5d6d30f
hho114 <hho114@csu.fulleton.edu>
2019-05-03 12:14:02 -0700

    update count grade

--- count_student_grade.php
+++ count_student_grade.php
@@ -1,5 +1,6 @@
 
 <?php
+
 $servername = "ecsmysql";
 $username = "cs332t15";
 $password = "Oaj2chea";
@@ -51,50 +52,6 @@ $sql = "SELECT DISTINCT enroll_grade, course_title, Count(enroll_grade) as 'Coun
 
 
 
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-  <div class="container-fluid bg-success text-center">
-    <h3> What total grades count in Course</h3>
-  </div>
-  
-  <table class="table table-striped table-bordered">
-    <thead>
-    <tr class="info text-center">
-    <th>Grades</th>
-
-      <?php
-    foreach($grades as $x => $x_value)
-    {
-      ?>
-      <th>  <?php  echo $x; ?> </th>
-
-    <?php } ?>
-    </tr>
-  </thead>
-
-    <tbody>
-      <tr>
-    <td>Number of Students </td>
-    <?php foreach ($grades as $i)
-    {?>
-        <td><?php echo $i; ?> </td>
-    <?php}?>
-      </tr>
-  </tbody>
-</table>
-
-
-</html>
-</body>
 <?php
  mysql_close($link);
 
